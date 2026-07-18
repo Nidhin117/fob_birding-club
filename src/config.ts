a6b987ebeadb365b376471ca309e3c2bb31b1cac
Nidhin117 <64641712+Nidhin117@users.noreply.github.com>
2026-07-19 00:05:15 +0530

    configurable params like IG profile

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Friends of Baanaadi';
+export const SITE_DESCRIPTION = 'A community of bird watching enthusiasts exploring Bengaluru and surrounding districts.';
+export const INSTAGRAM_URL = 'https://www.instagram.com/friendsofbaanaadi?igsh=NWc5NzhiMGs0bDQ1';
